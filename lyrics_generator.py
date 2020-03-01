e02e321f5e93b518440d008c865c919a996a46bf
YanaPalacheva <palachevayana@gmail.com>
2020-03-01 23:14:33 +0100

    documentation for LyricsNN and LyricsGenerator added

--- lyrics_generator.py
+++ lyrics_generator.py
@@ -9,6 +9,14 @@ from rhymer_syl import RhymerSyl
 
 class LyricsGenerator:
     def __init__(self, identifier, params, syllable_rhyme=False):
+        """
+        Initialization of LyricsGenerator class providing methods for generation of lyrics based on Markov chains and
+        RNN: it generates markov sequences based on word1->word2 transition probabilities (markovify library) and
+        uses an LSTM network to pick the most suitable sequence (LyricsNN class).
+        :param identifier: current artist or genre, str
+        :param params: contains depth, max_syllables, max_overlap, num_lines parameters, dict
+        :param syllable_rhyme: True if we use morpheme based rhyme, False if we use 2-last-letters based rhyme, bool
+        """
         self.params = params
         self.identifier = identifier
         if syllable_rhyme:
@@ -23,11 +31,20 @@ class LyricsGenerator:
         self.original_bars = split_file(self.training_file)
 
     def training_phase(self):
+        """
+        Training phase: creation of list of two 2-morpheme or 2-letter endings (based on training data)
+        and training of an LSTM network (self.lyrics_model, instance of LyricsNN class)
+        """
         rhyme_list = self.rhymer.rhymeindex(self.original_bars)
         x_data, y_data = self.build_dataset(self.original_bars, rhyme_list)
         self.lyrics_model.train(x_data, y_data)
 
     def generating_phase(self):
+        """
+        Generation phase: consecutive creation and filtering of markov sequences and vectors and
+        converting these vectors into lyrics.
+        :return: generated lyrics, str
+        """
         markov_bars = self.generate_lyrics()
         if os.path.exists(self.rhymer.rhyme_filename):
             rhyme_list = split_file(self.rhymer.rhyme_filename)
@@ -45,6 +62,13 @@ class LyricsGenerator:
         return lyrics_str
 
     def build_dataset(self, lines, rhyme_list):
+        """
+        Building datasets for LSTM training: one contains syllable and rhyme scores for odd couplets (two lines),
+        another - the same information about even couplets.
+        :param lines: training lyrics lines, list
+        :param rhyme_list: list two 2-morpheme or 2-letter endings (based on training data), list
+        :return datasets containing information about odd and even couplets (syllables and rhyme vectors), np array
+        """
         dataset = []
         for i, line in enumerate(lines):
             line_list = [self.syllables(line), self.rhymer.rhyme(i, line, rhyme_list)]
@@ -65,10 +89,15 @@ class LyricsGenerator:
         return x_data, y_data
 
     def generate_lyrics(self):
+        """
+        Generate a list of markov sequences (of size 5 * desired output size)
+        and filter out lines with repetitive last word (not more than 2 lines with similar last word are permitted)
+        :return: filtered markov sequences, list
+        """
         bars = []
         last_words = []
         overlap = self.params['max_overlap']
-        while len(bars) < self.params['num_lines']:
+        while len(bars) < self.params['num_lines'] * 5:
             bar = self.markov_model.make_sentence(max_overlap_ratio=overlap, tries=50)
             if bar and self.syllables(bar) < 1:
                 last_word = get_last_word(bar)
@@ -78,6 +107,11 @@ class LyricsGenerator:
         return bars
 
     def create_vectors(self, rhyme_list):
+        """
+        Generation of vectors with trained LSTM network based on initial lines and rhyme list.
+        :param rhyme_list: list of two 2-morpheme or 2-letter endings (based on training data), list
+        :return: list of vectors predicted by LSTM network
+        """
         lyrics_vectors = []
         initial_index = random.choice(range(len(self.original_bars) - 1))
         initial_lines = self.original_bars[initial_index:initial_index + 2]
@@ -91,9 +125,22 @@ class LyricsGenerator:
         return lyrics_vectors
 
     def vectors_into_lyrics(self, vectors, markov_bars, rhyme_list):
-        print("Writing verse:\n")
+        """
+        Producing a list of the generated lines based on vectors with the highest scores.
+        :param vectors: vectors predicted by LSTM network, list
+        :param markov_bars: list of filtered markov sequences, list
+        :param rhyme_list: list of two 2-morpheme or 2-letter endings, list
+        :return: generated lyrics lines, list
+        """
+        print("Writing lyrics:\n")
 
         def last_word_compare(lyrics, line2):
+            """
+            Calculation of penalty score for lines within generated lyrics having the similar to line2 last word.
+            :param lyrics: list of generated lines, list
+            :param line2: generated line to compare
+            :return: penalty score, float
+            """
             penalty = 0
             for line1 in lyrics:
                 word1 = get_last_word(line1)
@@ -103,6 +150,14 @@ class LyricsGenerator:
             return penalty
 
         def calculate_score(vector_half, syllables, rhyme, penalty):
+            """
+            Calculation of a final score for the generated line.
+            :param vector_half: vector containing syllable and rhyme score for a line, np array
+            :param syllables: syllable score of the generated line, float
+            :param rhyme: rhyme score of the generated line, float
+            :param penalty: penalty score for repetitive last words within lyrics, float
+            :return: final score, float
+            """
             desired_syllables = vector_half[0]
             desired_rhyme = vector_half[1]
             desired_syllables = desired_syllables * self.params['max_syllables']
@@ -138,7 +193,7 @@ class LyricsGenerator:
             for item in scorelist:
                 if item[1] == max_score:
                     lyrics.append(item[0])
-                    print(str(item[0]))
+                    print(str(item[0]))  # writing generated lyrics into console
                     for i in dataset:
                         if item[0] == i[0]:
                             dataset.remove(i)
@@ -147,5 +202,10 @@ class LyricsGenerator:
         return lyrics
 
     def syllables(self, line):
+        """
+        Calculation of a syllable score for a line: num of syllables in the line / max_syllables param
+        :param line: target line, str
+        :return: syllable score, float
+        """
         count = count_syllables(line)
         return count / self.params['max_syllables']
--- nn_model.py
+++ nn_model.py
@@ -7,6 +7,11 @@ from keras.layers.core import Dense
 
 class LyricsNN:
     def __init__(self, depth, identifier):
+        """
+        Initialization of LyricsNN class describing an RNN for lines prediction.
+        :param depth: number of NN hidden layers, int
+        :param identifier: current artist or genre, str
+        """
         self.model = Sequential()
         self.identifier = identifier
         self.model_filename = f'models/{self.identifier}.model'
@@ -14,20 +19,32 @@ class LyricsNN:
         for i in range(depth):
             self.model.add(LSTM(8, return_sequences=True))
         self.model.add(LSTM(2, return_sequences=True))
-        # self.model.summary()
         self.model.compile(optimizer='rmsprop',
                            loss='mse')
 
     def load_model(self):
-        if self.model_filename in os.listdir("."):
+        """
+        Loading of the trained model from file (stored in 'models' directory).
+        """
+        if os.path.exists(self.model_filename):
             self.model.load_weights(self.model_filename)
 
     def train(self, x_data, y_data):
+        """
+        Training of the model on 2 datasets.
+        :param x_data: dataset containing information about odd couplets (syllables and rhyme vectors), np array
+        :param y_data: dataset containing information about even couplets (syllables and rhyme vectors), np array
+        """
         self.model.fit(np.array(x_data), np.array(y_data),
                        batch_size=2,
                        epochs=3,
                        verbose=1)
         self.model.save_weights(self.model_filename)
 
-    def predict(self, input):
-        return self.model.predict(input)
+    def predict(self, input_vec):
+        """
+        Prediction function based on an input vector.
+        :param input_vec: vector containing information on syllables and rhyme, np array of size (1, 2, 2)
+        :return: predicted vector
+        """
+        return self.model.predict(input_vec)
