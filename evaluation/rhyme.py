ae632a84e2c8e7a16be56bd350d11d38fdcf4320
YanaPalacheva <palachevayana@gmail.com>
2020-03-02 02:25:44 +0100

    grammar evaluation refactored

--- evaluation/rhyme.py
+++ evaluation/rhyme.py
@@ -1,19 +1,19 @@
 def rhyme_score(endings):
     """
-    Computes two rhyme scores: rhyme percentage and rhyming lines percentage.
+    Computes two rhyme scores: rhyme score and rhyming lines score.
 
-    Rhyme percentage is defined as following:
+    Rhyme score is defined as following:
         rhyme_count / (2*line_num/4),
     where rhyme_count is the number of existing rhymes within 4-line chunks,
     (2*line_num/4) is the maximum ("perfect") number of rhymes in lyrics:
     line_num/4 is the number of 4-line chunks in lyrics,
     2 is the maximum number of different rhymes in a 4-line chunk (aabb, abab).
 
-    Rhyming lines percentage is a number of lines that rhymed with another line within the 4-line chunk
+    Rhyming lines score is a number of lines that rhymed with another line within the 4-line chunk
     divided by total number of lines.
 
     :param endings: list of endings for the last word of each line, str[].
-    :return: two rhyme scores: rhyme percentage and rhyming lines percentage.
+    :return: two rhyme scores: rhyme score and rhyming lines score, both values are in range (0,1).
     """
     line_num = len(endings)
     rhymes_count = 0
--- grammar_evaluation.py
+++ grammar_evaluation.py
@@ -1,195 +1,86 @@
 from gingerit.gingerit import GingerIt
-
 import language_check
 
-def ginger_errors(line, Typos = False):
-    """Computes number of grammatical errors in a line of lyrics based on Gingerit parser
 
-Args:
-    line (str): a line of lyrics represented as a string value
-    Typos (bool): a boolean value specifying whether to include typos
-
-Returns:
-    number of errors (int)
-"""
-    parser = GingerIt()
-    parse = parser.parse(line)
-    errors = parse['corrections']
-    
-    nontypos = [d for d in errors if d['definition'] != None]
-    
-    if Typos:
+def tool_errors(line,  allow_typos=False, parser=None):
+    """
+    Computes number of grammatical errors in a line of lyrics
+        :param line: a line of lyrics represented as a string value, str
+        :param allow_typos: a boolean value specifying whether to include typos, bool
+        :param parser: parser instance, GingerIt; None for LanguageTool
+        :return: number of errors, int
+    """
+    if parser:
+        parse = parser.parse(line)
+        errors = parse['corrections']
+        nontypos = [err for err in errors if err['definition']] # and not err['definition'].endswith(' not')]
+    else:
+        tool = language_check.LanguageTool('en-US')
+        errors = tool.check(line)
+        nontypos = [err for err in errors if err.category != 'Possible Typos']
+    if allow_typos:
         return len(errors)
     else:
         return len(nontypos)
-    
-def mean_ginger_error(lyrics, Typos = False):
-    """Computes average number of grammatical errors in an entire lyrics based on Gingerit parser
 
 
-Args:
-    lyrics (str[]): a list of lyrics lines
-    Typos (bool): a boolean value specifying whether to include typos
-
-Returns:
-    average number of errors per line (float)
-"""
+def mean_error(lyrics, allow_typos=False, parser=None, combined=False):
+    """
+    Computes average number of grammatical errors in an entire lyrics based on Gingerit parser
+    :param lyrics: a list of lyrics lines, str[]
+    :param allow_typos: a boolean value specifying whether to include allow_typos, bool
+    :param parser: parser instance, GingerIt; None for LanguageTool
+    :param combined: a boolean value specifying whether to combine tools, bool
+    :return: average number of errors per line, float
+    """
     total = 0
-    # We have assumed lyrics is a list of strings corresponding to each line
     for line in lyrics:
-        total += ginger_errors(line, Typos)
-    return total/len(lyrics)
+        if parser:
+            if combined:
+                total += tool_errors(line, allow_typos=True, parser=parser) - lang_typos(line)
+            else:
+                total += tool_errors(line, parser=parser, allow_typos=allow_typos)
+        else:
+            total += tool_errors(line, parser=None, allow_typos=allow_typos)
 
-def ginger_file(filename, Typos = False):
-"""Prints average number of grammatical errors per line of lyrics based on Gingerit parser
-
-
-Args:
-    filename (str): name of file containing lyrics
-    Typos (bool): a boolean value specifying whether to include typos
-
-Returns:
-    None
-"""
-
-    f = open(filename, "r")
-    lyrics = f.readlines() 
-    score = mean_ginger_error(lyrics, Typos)
-    print(filename, "has mean grammatical error ", score)
-    
-
-def lang_errors(line, Typos = False):
-    """Computes number of grammatical errors in a line of lyrics based on language_check parser
-
-Args:
-    line (str): a line of lyrics represented as a string value
-    Typos (bool): a boolean value specifying whether to include typos
-
-Returns:
-    number of errors (int)
-"""
-    tool = language_check.LanguageTool('en-US')
-    
-    matches = tool.check(line)
-    nontypos = [match for match in matches if match.category != 'Possible Typos']
-    
-    if Typos:
-        return len(matches)
-    else:
-        return len(nontypos)
-    
-def mean_lang_error(lyrics, Typos = False):
-    """Computes average number of grammatical errors in an entire lyrics based on language_check parser
-
-
-Args:
-    lyrics (str[]): a list of lyrics lines
-    Typos (bool): a boolean value specifying whether to include typos
-
-Returns:
-    average number of errors per line (float)
-"""
-
-    total = 0
-    # We have assumed lyrics is a list of strings corresponding to each line
-    for line in lyrics:
-        total += lang_errors(line, Typos)
-    return total/len(lyrics)
-
-def lang_file(filename, Typos = False):
-"""Prints average number of grammatical errors per line of lyrics based on language_check parser
-
-
-Args:
-    filename (str): name of file containing lyrics
-    Typos (bool): a boolean value specifying whether to include typos
-
-Returns:
-    None
-"""
-
-    f = open(filename, "r")
-    lyrics = f.readlines() 
-    score = mean_lang_error(lyrics, Typos)
-
+    return total / len(lyrics)
 
 
 def lang_typos(line):
-"""Computes number of typos in a line of lyrics based on language_check parser
-
-
-Args:
-    line (str): line of lyrics represented as string value
-
-
-Returns:
-    number of typos (int)
-"""
-
-    
+    """
+    Computes number of typos in a line of lyrics based on language_check parser
+    :param line: line of lyrics represented as string value, str
+    :return: number of typos, int
+    """
     tool = language_check.LanguageTool('en-US')
     matches = tool.check(line)
     typos = [match for match in matches if match.category == 'Possible Typo' or match.category == 'Possible Typos']
-    
     return len(typos)
 
 
-# removal either by 'ginger' or by 'lang'
-def nontypo_errors(line, remove = 'ginger'):
-"""Computes number of non-typo errors in a line of lyrics by first counting all errors with Gingerit parser, then subtracting typos by either Gingerit of language_check parser
-
-
-Args:
-    line (str): line of lyrics represented as string value
-    remove (str): name of parser used to remove typos; either 'ginger' or 'lang'
-
-
-Returns:
-    number of non-typo errors (int)
-"""
-
-    if remove == 'ginger':
-        return ginger_errors(line)
-    if remove == 'lang':
-        return ginger_errors(line, Typos = True) - lang_typos(line)
-    print('Error: undefined removal')
-    return None
-
-
-def nontypo_mean(lyrics, remove = 'ginger'):
-"""Computes average number of non-typo errors per line in lyrics using the nontypo_errors method.
-
-
-Args:
-    lyrics (str[]): list of lyrics lines
-    remove (str): name of parser used to remove typos; either 'ginger' or 'lang'
-
-
-Returns:
-    average number of non-typo errors (float)
-"""
-
-    total = 0
-    for line in lyrics:
-        total += nontypo_errors(line, remove)
-    return total / len(lyrics)
-
-def nontypo_file(filename, remove = 'ginger'):
-"""Prints average number of grammatical errors per line of lyrics based on the nontypo_errors method.
-
-
-Args:
-    filename (str): name of file containing lyrics
-    remove (str): name of parser used to remove typos; either 'ginger' or 'lang'
-
-Returns:
-    None
-"""
-
-    f = open(filename, "r")
-    lyrics = f.readlines() 
-    score = nontypo_mean(lyrics, remove)
+def get_grammar_score(filename, allow_typos, parser, combined):
+    """
+    Returns grammar score of the given file based on score method
+    :param filename: name of file containing lyrics, str
+    :param allow_typos: a boolean value specifying whether to include typos, bool
+    :param parser: parser instance, GingerIt; None for LanguageTool
+    :param combined: a boolean value specifying whether to combine tools, bool
+    :return: score, float
+    """
+    with open(filename, "r") as f:
+        lyrics = f.readlines()
+    score = mean_error(lyrics[:100], allow_typos=allow_typos, parser=parser, combined=combined)
     print(filename, "has mean grammatical error ", score)
+    return score
+
+
+ginger_parser = GingerIt()
+print(get_grammar_score('data/50-cent.txt', allow_typos=True, parser=ginger_parser, combined=False))
+"""
+freddie: allow_typos=False, 0.29 / 0.29 with and without consideration of ' not'
+50-cent: allow_typos=False, 0.56 / 0.67 with and without consideration of ' not'
+"""
+
 
 
 
